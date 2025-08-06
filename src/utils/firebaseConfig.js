190908ed7f01e57e5449cfc7c9f842f20d94b5a1
Lucas Lira <67843377+LucasLiraa@users.noreply.github.com>
2025-08-06 00:07:36 -0300

    Update firebaseConfig.js

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,8 +17,7 @@ const firebaseConfig = {
   measurementId: "G-35T3ZLR05K"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app); // ✅ Aqui você passa o app
 
-export { app };
+export { db };
