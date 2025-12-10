67920b809ec0204bde44199fbbd153943f76d183
Lucas Lira <67843377+LucasLiraa@users.noreply.github.com>
2025-12-10 13:33:06 -0300

    Add files via upload

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import "./styles/profile.css";
 
@@ -41,48 +40,4 @@ function ProfileUser() {
   );
 }
 
-=======
-import React, { useState } from "react";
-import "./styles/profile.css";
-
-function ProfileUser() {
-  const [status, setStatus] = useState("Disponível");
-
-  return (
-    <div className="profileUser">
-
-      <div className="profileHeader">
-        <h3>Meu perfil</h3>
-      </div>
-
-      <div className="profileCard">
-        {/* Cabeçalho do card */}
-        <div className="profileHeaderCard">
-
-          <div className="profileImage">
-            <img src="/profile-dr-paulo.jpeg" alt="Foto de perfil" />
-          </div>
-
-          <div className="profileInfo">
-            <h4 className="profileName">Dr. Paulo Vasconcelos</h4>
-            <p className="profileInfo">Cirurgião Plástico</p>
-            <p className="profileInfo">R. Voluntários da Pátria, 560 - Sala 705</p>
-          </div>
-        </div>
-
-        {/* Card próxima cirurgia */}
-        <div className="profileSubCardMain">
-            <div className="profileSubCard">
-                <h5>Próxima Cirurgia</h5>
-                <p>Elza Pereira dos Santos</p>
-                <p>03/12/2025 às 11:00</p>
-            </div>
-        </div>
-
-      </div>
-    </div>
-  );
-}
-
->>>>>>> e432c81afbfcf20f9e060b437fc8dcafc90f5cf5
 export default ProfileUser;
